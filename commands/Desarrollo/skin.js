a39d384ac92374863e55e6b899867ad6dcb04ff5
Didacus12 <diego.hernandez@colegiomiralmonte.es>
2021-10-08 15:24:42 +0200

    Try to fix .skin command

--- commands/Desarrollo/skin.js
+++ commands/Desarrollo/skin.js
@@ -1,11 +1,7 @@
 require("dotenv").config();
 const Discord = require("discord.js");
 const Command = require("../../structures/Commandos.js");
-//const { MessageButton, MessageActionRow } = require("discord-buttons");
-require("discord-reply");
-let descripcion, usage;
-const fs = require("fs");
-let encendido = false;
+const { MessageEmbed } = require("discord.js");
 
 module.exports = class Skin extends Command {
   constructor(client) {
@@ -18,106 +14,24 @@ module.exports = class Skin extends Command {
       usage: ["<@user>", "<@usuario>"],
       args: true,
       category: "Utils",
-      inactive: true,
+      cooldown: 1,
+      inactive: false,
     });
   }
   async run(client, message, args, prefix, lang, webhookClient, ipc) {
     try {
-      const cabeza = new Discord.MessageEmbed()
-        .setColor("#DD8811")
-        .setImage(`https://mc-heads.net/head/${args[0]}`);
-        const cuerpo = new Discord.MessageEmbed()
-        .setColor("#DD8811")
-        .setImage(`https://mc-heads.net/head/${args[0]}`);
-        const avatar = new Discord.MessageEmbed()
-        .setColor("#DD8811")
-        .setImage(`https://mc-heads.net/head/${args[0]}`);
-        const jugador = new Discord.MessageEmbed()
-        .setColor("#DD8811")
-        .setImage(`https://mc-heads.net/head/${args[0]}`);
-
-      let ButtonArray = [cabeza, cuerpo, avatar, jugador];
-
-      const embed = new Discord.MessageEmbed()
+      let embed = new Discord.MessageEmbed()
         .setColor(process.env.EMBED_COLOR)
-        .setTitle(`${args[0]}`)
-        .setDescription(
-          `<a:828830816486293608:836296002893381682> Aquí encontraras las distintas opciones que puedes elegir sobre la skin del jugador que has seleccionado.`
-        )
-        .addField(
-          "Categorias",
-          "Las distintas opciones a elegir están divididas en categorías. Solo pulsa una y te mostrará lo relacionado de dicha categoría."
-        )
-        .setThumbnail(
-          message.author.avatarURL({
-            dynamic: true,
-          })
-        );
-      console.log("3");
-
-      //message.lineReply('¡Te he enviado un mensaje privado con mis comandos!')
-      message.channel.send({
-        embed: embed,
-        buttons: ButtonArray,
-      });
-      console.log("4");
-      if (encendido == false) {
-        client.on("clickButton", async (button, err) => {
-          if (err) return;
-          console.log("5");
-          if (button.id === "bu1") {
-            const embed = new Discord.MessageEmbed()
-              .setColor("#DD8811")
-              .setImage(`https://mc-heads.net/head/${args[0]}`);
-            await button.reply.send("", {
-              embed: embed,
-              ephemeral: true,
-            });
-            console.log("6");
-            return;
-          } else if (button.id === "bu2") {
-            const embed = new Discord.MessageEmbed()
-              .setColor("#DD8811")
-              .setImage(`https://mc-heads.net/body/${args[0]}`);
-            await button.reply.send("", {
-              embed: embed,
-              ephemeral: true,
-            });
-            console.log("7");
-            return;
-          } else if (button.id === "bu3") {
-            const embed = new Discord.MessageEmbed()
-              .setColor("#DD8811")
-              .setImage(`https://mc-heads.net/avatar/${args[0]}`);
-            await button.reply.send("", {
-              embed: embed,
-              ephemeral: true,
-            });
-            console.log("8");
-            return;
-          } else if (button.id === "bu4") {
-            const embed = new Discord.MessageEmbed()
-              .setColor("#DD8811")
-              .setImage(`https://mc-heads.net/player/${args[0]}`);
-            await button.reply.send("", {
-              embed: embed,
-              ephemeral: true,
-            });
-            console.log("9");
-            return;
-          }
-          console.log("Saliendo");
-        });
-      } else return;
+        .setTitle(`Skin de ${args[0]}`)
+        .setImage(`https://mc-heads.net/player/${args[0]}`)
+      return message.channel.send({embeds: [embed]});
     } catch (e) {
-      console.error(e);
       message.channel.send({
         embeds: [
           new Discord.MessageEmbed()
-          .setColor("RED")
-          .setTitle(client.language.ERROREMBED)
-          .setDescription(client.language.fatal_error)
-          .setFooter(message.author.username, message.author.avatarURL())
+          .setColor(process.env.EMBED_COLOR)
+          .setTitle(`Skin de ${args[0]}`)
+          .setImage(`https://mc-heads.net/player/${args[0]}`)
         ]
       });
       webhookClient.send(
